b8ddcd8ebcc6b65aa15a0ac6e813467fb97bc449
tyrus <tyrushenry03@gmail.com>
2023-10-06 13:06:55 -0400

    created logic up to fetch request

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+
+const api = {
+  key: "2983bd3b52b446b8a24e7adde78a747d",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+function App() {
+
+  const [search, setSearch] = useState("");
+  const searchPressed = () => {
+    console.log("ok!")
+  };
+  
+  return (
+    <div className="App">
+      <h1>GYATTT</h1> 
+      <input type="text" placeholder="Search..." onChange={(e) => setSearch(e.target.value)}/>
+      <button onClick={searchPressed}>Search</button>
+    </div>
+  );
+}
+
+export default App;
