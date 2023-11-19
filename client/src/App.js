5bc27a7bd93b1bcd023dda8b908e9d90c9037917
tyrus <tyrushenry03@gmail.com>
2023-11-19 10:24:35 -0500

    current

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,60 @@
-import { Container } from '@mui/material';
 import './App.css';
-
-import React from 'react';
-import { useState } from 'react';
-// import Search from './components/Search/Search';
-// import Home from './components/Home/Home';
+import React, { useState } from 'react';
 
 const api = {
-  key: `${process.env.REACT_APP_API_KEY}`,
+  key: process.env.REACT_APP_API_KEY,
   base: "https://api.openweathermap.org/data/2.5/",
 };
 
 function App() {
-
   const [search, setSearch] = useState("");
-  const [weather, setWeather] = useState({});
+  const [forecast, setForecast] = useState({});
 
   const searchPressed = () => {
-    fetch(`${api.base}weather?q=${search}&units=imperial&APPID=${api.key}`)
+    fetch(`${api.base}forecast?q=${search}&units=imperial&cnt=5&APPID=${api.key}`)
       .then(res => res.json())
-      .then((result) => {
-        setWeather(result);
+      .then(result => {
+        setForecast(result);
+        console.log(result);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
       });
   };
 
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <div className='input-box'>
-        <input type="text" placeholder="Search..." onChange={(e) => setSearch(e.target.value)}/>
-        <button className='search-button' onClick={searchPressed}>Search</button>
-      </div>
+      <h1 className='title'>Open Air Weather</h1>
+      <div className='container'>
+        <div className='input-box'>
+          <input type="text" placeholder="Search..." onChange={(e) => setSearch(e.target.value)} />
+          <button className='search-button' onClick={searchPressed}>Search</button>
+        </div>
 
-      {/* result container held by terinary operator(preventing undefined error) */}
-      {typeof weather.main !== "undefined" ? (
-        <div className='result-container'>
-        <p>{weather.name}</p>
-        <p>{weather.main.temp}</p>
-        <p>{weather.weather[0].main}</p>
-        <p>({weather.weather[0].description})</p>
+        {forecast.list && forecast.list.length > 0 ? (
+          <div className='result-container'>
+            <div className='current-weather'>
+              <p>Current Weather: {forecast.list[0].main.temp}&deg;F</p>
+              <p>Feels Like: {forecast.list[0].main.feels_like}&deg;F</p>
+              <p>High: {forecast.list[0].main.temp_max}&deg;F</p>
+              <p>Low: {forecast.list[0].main.temp_min}&deg;F</p>
+            </div>
+            {forecast.list.map(item => (
+              <div key={item.dt} className='next-hour'>
+                <p>{new Date(item.dt * 1000).toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' })}</p>
+                <p>{item.main.temp}</p>
+                <p>{item.weather[0].main}</p>
+                {/* <p>({item.weather[0].description})</p> */}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p></p>
+        )}
+
       </div>
-      ) : ("")}
     </div>
-    // <Home/>
-    // <Search/>
-  )
-};
+  );
+}
 
 export default App;
