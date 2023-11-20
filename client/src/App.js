b21f92d8a912d6f0dd1fde5b44627490e461b0ed
tyrus <tyrushenry03@gmail.com>
2023-11-20 10:25:12 -0500

    css progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { useState } from 'react';
+import SearchIcon from './components/Icons/searchicon';
 
 const api = {
   key: process.env.REACT_APP_API_KEY,
@@ -27,8 +28,19 @@ function App() {
       <h1 className='title'>Open Air Weather</h1>
       <div className='container'>
         <div className='input-box'>
-          <input type="text" placeholder="Search..." onChange={(e) => setSearch(e.target.value)} />
-          <button className='search-button' onClick={searchPressed}>Search</button>
+          <input
+            className='search-bar'
+            type="text"
+            placeholder="Search..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                searchPressed();
+              }
+            }}
+          />
+          <button className='search-button' onClick={searchPressed}><svg className='search-icon' xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" /></svg></button>
         </div>
 
         {forecast.list && forecast.list.length > 0 ? (
@@ -39,14 +51,16 @@ function App() {
               <p>High: {forecast.list[0].main.temp_max}&deg;F</p>
               <p>Low: {forecast.list[0].main.temp_min}&deg;F</p>
             </div>
+            <div className='forecast-container'>
             {forecast.list.map(item => (
-              <div key={item.dt} className='next-hour'>
-                <p>{new Date(item.dt * 1000).toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' })}</p>
-                <p>{item.main.temp}</p>
-                <p>{item.weather[0].main}</p>
-                {/* <p>({item.weather[0].description})</p> */}
+                <div key={item.dt} className='forecast-items'>
+                  <p>{new Date(item.dt * 1000).toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' })}</p>
+                  <p>{item.main.temp}</p>
+                  <p>{item.weather[0].main}</p>
+                  {/* <p>({item.weather[0].description})</p> */}
               </div>
             ))}
+            </div>
           </div>
         ) : (
           <p></p>
